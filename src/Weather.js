945a41286a548475cf0a7ccfa19d81e0be61bdbf
Vanessa Y <75158000+vwt604@users.noreply.github.com>
2021-12-10 09:57:11 -0800

    Refactor weather info to be a separate component

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   let [loaded, setLoaded] = useState(false);
   let [weather, setWeather] = useState({});
+  let [city, setCity] = useState("Vancouver");
 
   const showWeather = (res) => {
     setWeather({
@@ -19,72 +21,46 @@ export default function Weather(props) {
     console.log(res);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCity(event.target[0].value);
+  };
+
   if (loaded) {
     return (
       <div>
-        <form className="weather-search" id="weather-search">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control search-input"
-                id="search-input"
-              />
-            </div>
-            <div className="col-3 p-0">
-              <input
-                type="submit"
-                className="btn btn-primary w-100 search-btn"
-                value="Search"
-              />
-            </div>
-          </div>
-        </form>
-        <div className="weather">
-          <div className="row">
-            <div className="col-6">
-              <h1 className="city">{props.city}</h1>
-              <ul className="normal">
-                <li>
-                  <span className="weather-condition">
-                    {weather.description}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Feels like:{" "}
-                    <span className="weather-feels-like">
-                      {Math.round(weather.feelsLike)}°C
-                    </span>
-                  </span>
-                </li>
-                <li>
-                  Last updated: <span className="time">Friday 15:49</span>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6">
-              <div className="temperature-container d-flex justify-content-end">
-                <img
-                  className="weather-icon"
-                  src={`http://openweathermap.org/img/wn/${weather.icon}.png`}
-                  alt="clear"
+        <div>
+          <form
+            className="weather-search"
+            id="weather-search"
+            onSubmit={handleSubmit}
+          >
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city.."
+                  className="form-control search-input"
+                  id="search-input"
+                  // onClick={handleSubmit}
                 />
-                <span className="temperature">
-                  {Math.round(weather.temperature)}
-                </span>
-                <span className="unit">°C</span>
+              </div>
+              <div className="col-3 p-0">
+                <input
+                  type="submit"
+                  className="btn btn-primary w-100 search-btn"
+                  value="Search"
+                />
               </div>
             </div>
-          </div>
+          </form>
         </div>
+        <WeatherInfo weather={weather} city={city} />
       </div>
     );
   } else {
     const apiKey = "ce7f3cb6658fc606e9cb42397a4c2dd1";
-    let city = "Vancouver";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(apiUrl).then(showWeather);
     return "Loading...";
   }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="weather">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="city">{props.city}</h1>
+          <ul className="normal">
+            <li>
+              <span className="weather-condition">
+                {props.weather.description}
+              </span>
+            </li>
+            <li>
+              <span>
+                Feels like:{" "}
+                <span className="weather-feels-like">
+                  {Math.round(props.weather.feelsLike)}°C
+                </span>
+              </span>
+            </li>
+            <li>
+              Last updated: <span className="time">Friday 15:49</span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <div className="temperature-container d-flex justify-content-end">
+            <img
+              className="weather-icon"
+              src={`http://openweathermap.org/img/wn/${props.weather.icon}.png`}
+              alt="clear"
+            />
+            <span className="temperature">
+              {Math.round(props.weather.temperature)}
+            </span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
