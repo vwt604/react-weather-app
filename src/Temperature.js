293eae758d7030d96a1aa222f76792cf32115da4
Vanessa Y <75158000+vwt604@users.noreply.github.com>
2021-12-28 13:29:03 -0800

    Fix temperature conversion

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,10 +1,33 @@
 import React, { useState } from "react";
 
 export default function Temperature(props) {
+  const [unit, setUnit] = useState(props.temperature);
+
+  const setCelcius = (e) => {
+    e.preventDefault();
+    setUnit(props.temperature);
+  };
+
+  const toFarenheit = (temp) => {
+    return Math.round(temp * (9 / 5) + 32);
+  };
+
+  const setFarenheit = (e) => {
+    e.preventDefault();
+    setUnit(toFarenheit(props.temperature));
+  };
   return (
     <div className="temperature-container d-flex justify-content-end">
-      <span className="temperature">{props.temperature}</span>
-      <span className="unit">°C</span>
+      <span className="temperature">{unit}°</span>
+      <span className="unit">
+        {" "}
+        <a href="/" onClick={setCelcius}>
+          C
+        </a>{" "}
+        <a href="/" onClick={setFarenheit}>
+          F
+        </a>
+      </span>
     </div>
   );
 }
